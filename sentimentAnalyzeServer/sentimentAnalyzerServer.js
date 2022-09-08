acf843f1ef040b7da5e0eea17ed89688e0badecc
tswede <112585892+tswede@users.noreply.github.com>
2022-09-08 12:33:13 -0500

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -12,8 +12,8 @@ app.use(cors_app());
 /*Uncomment the following lines to loan the environment 
 variables that you set up in the .env file*/
 
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const api_key = process.env.API_KEY;
 const api_url = process.env.API_URL
